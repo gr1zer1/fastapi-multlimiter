0e72bf6be4eee865d29544067dadc08acd33536d
gr1zer1 <timuravramenko99@gmail.com>
2026-05-08 20:40:26 +0200

    fix: Redis Backend

--- backend/redis_backend.py
+++ backend/redis_backend.py
@@ -12,34 +12,64 @@ class RedisBackend(BaseBackend):
         self.expire: int | None = None
         self.client: redis.Redis = redis.from_url(url)
 
+ 
     async def get(self, key: str) -> dict | None:
-        response =  self.client.get(f"fw:{key}")
-        if response is not None:
-            # decoded_response = json.loads(response)
-            return {"counter":response}
-        else:
-            return None
-        
+        redis_key = f"fw:{key}"
 
+        value = await self.client.get(redis_key)
+
+        if value is None:
+            return None
+
+        ttl = await self.client.ttl(redis_key)
+
+        return {
+            "counter": int(value),
+            "ttl": ttl,
+        }
 
     async def put(self, key: str) -> dict:
-        if await self.get(key) is not None:
+        redis_key = f"fw:{key}"
 
+        response = await self.get(key)
+
+        if response is not None:
             return await self.increment(key)
 
-        # payload = {"counter":1,"expire_at":datetime.now(timezone.utc) + timedelta(seconds=self.expire)}
-        # value = json.dumps(payload)
-        
-        self.client.set(f"fw:{key}", 1, self.expire)
-        return {"counter":1}
+        await self.client.set(
+            redis_key,
+            1,
+            ex=self.expire,
+        )
 
+        return {
+            "counter": 1,
+            "ttl": self.expire,
+        }
 
-    async def increment(self, key: str) -> int | dict:
-        res = await self.get(key)
-        
-        if res is not None:
-            
-            self.client.set(f"fw:{key}", res+1, keepttl=True)
-        else:
-            return await self.put(key)
+    async def increment(self, key: str) -> dict:
+        redis_key = f"fw:{key}"
 
+        value = await self.client.incr(redis_key)
+
+        if value == 1:
+            await self.client.expire(
+                redis_key,
+                self.expire,
+            )
+
+        ttl = await self.client.ttl(redis_key)
+
+        return {
+            "counter": value,
+            "ttl": ttl,
+        }
+
+    async def append(self, key, timestamp):
+        pass
+
+    async def get_range(self, key, from_time):
+        pass
+
+    async def _clear(self):
+        pass
