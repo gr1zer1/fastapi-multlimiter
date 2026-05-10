0693b4314e2e835dbac8fb156c302cb69c86d645
gr1zer1 <timuravramenko99@gmail.com>
2026-05-10 12:26:52 +0200

    add: cleanup func for in memory backend

--- backend/memory_backend.py
+++ backend/memory_backend.py
@@ -56,7 +56,7 @@ class MemoryBackend(BaseBackend):
         res = self.counter.get(f"sw:{key}")
         if res is None:
             return [] 
-
+        await self.cleanup(key)
         return  list(filter(lambda t: t>from_time,res))
     
 
@@ -71,9 +71,24 @@ class MemoryBackend(BaseBackend):
         time = datetime.now(timezone.utc) - timedelta(seconds=self.expire)
         
         res_range = await self.get_range(key,time.timestamp())
+        if not res_range:
+            return 0.0
+        
         timestamp = res_range[0]
 
         return (timestamp + self.expire) - datetime.now(timezone.utc).timestamp()
+
+    async def cleanup(self,key: str):
+        res = self.counter.get(f"sw:{key}")
+        if res is None:
+        
+            return
+        now = datetime.now(timezone.utc).timestamp()
+
+        for i in range(len(res) - 1, -1, -1):
+            if res[i] < now - self.expire - 1:
+                res.pop(i)
+    
 
 
     async def _clear(self):
